07691e6e0b07c0ddfe974f667c2fcbab2c5e77a9
Tomusdrw <tomusdrw@gmail.com>
2016-04-06 09:26:21 +0200

    Adding menu mockup

--- client/components/Header/index.js
+++ client/components/Header/index.js
@@ -9,6 +9,23 @@ export default class Header extends Component {
           <h1><span>Status</span> <small>Page</small></h1>
           <h2>{this.props.nodeName}</h2>
         </hgroup>
+        <div className='dapp-flex-item'></div>
+        <nav>
+          <ul>
+            <li>
+              <a href='#' className='active'>
+                <i className='icon-globe'></i>
+                <span>Status</span>
+              </a>
+            </li>
+            <li>
+              <a href='#'>
+                <i className='icon-users'></i>
+                <span>Accounts</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   }
