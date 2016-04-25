89a1d38b1816872fcb47d7eb713ba75163a4dba6
Adam Goldman <adamgoldman7@gmail.com>
2016-04-26 00:09:00 +0300

    add ut to toastr middleware + small refactor

--- client/middleware/toastr.js
+++ client/middleware/toastr.js
@@ -3,22 +3,45 @@ import {removeToast} from '../actions/toastr';
 
 export default class ToastrMiddleware {
 
+  constructor (time = 4000) {
+    this._time = time;
+  }
+
   toMiddleware () {
     return store => next => action => {
-      if (action.type !== 'add toast') {
-        return next(action);
+      let delegate;
+      switch (action.type) {
+        case 'add toast': delegate = ::this.onAddToast; break;
+        default:
+          next(action);
+          return;
       }
 
-      if (typeof action.payload === 'string') {
-        action.payload = {
-          message: action.payload
-        };
+      if (!delegate) {
+        return;
       }
 
-      action.payload.toastNo = store.getState().toastr.toastNo;
-
-      setTimeout(() => store.dispatch(removeToast(action.payload.toastNo)), 4000);
-      next(action);
+      delegate(store, next, action);
     };
+  }
+
+  onAddToast (store, next, action) {
+    action.payload = this.ensureObject(action.payload);
+
+    action.payload.toastNo = store.getState().toastr.toastNo;
+
+    setTimeout(() => store.dispatch(removeToast(action.payload.toastNo)), this._time);
+    next(action);
+  }
+
+  // allow shorthand by passing just string to "add toast" action
+  ensureObject (any) {
+    if (typeof any === 'string') {
+      any = {
+        message: any
+      };
+    }
+
+    return any;
   }
 }
