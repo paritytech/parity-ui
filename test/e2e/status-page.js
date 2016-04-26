36eff6b09c661fb1364be943e9f99f056445c790
Adam Goldman <adamgoldman7@gmail.com>
2016-04-26 12:55:32 +0300

    status page e2e test WIP

--- test/e2e/status-page.js
+++ test/e2e/status-page.js
@@ -35,29 +35,75 @@ module.exports = {
 
   'Assert network' (client) {
     [
-      // { selector: 'chain', name: '' },
-      // { selector: 'network-port', name: '' },
-      // { selector: 'rpc-enabled', name: '' },
-      // { selector: 'rpc-interface', name: '' },
-      // { selector: 'rpc-port', name: 'eth_hashrate' }
+      { selector: 'chain', name: 'ethcore_netChain' },
+      { selector: 'network-port', name: 'ethcore_netPort' },
       { selector: 'peers', name: 'net_peerCount' }
     ].map(assertNetwork.bind(client));
   },
 
-  'change extradata' (client) {
-    // submit/cancel are hidden on load, edit button displayed
-    client.expect.element(el('MiningSettings-extra-data-submit')).to.not.be.present;
-    client.expect.element(el('MiningSettings-extra-data-cancel')).to.not.be.present;
-    client.expect.element(el('MiningSettings-extra-data-edit')).to.be.present;
-    // switch to edit mode
-    client.click(el('MiningSettings-extra-data-edit'));
-    // submit/cancel displayed, edit button hidden
-    client.expect.element(el('MiningSettings-extra-data-submit')).to.be.present;
-    client.expect.element(el('MiningSettings-extra-data-cancel')).to.be.present;
-    client.expect.element(el('MiningSettings-extra-data-edit')).to.not.be.present;
+  'Assert rpc' (client) {
+    [
+      { selector: 'rpc-enabled', name: 'enabled' },
+      { selector: 'rpc-interface', name: 'interface' },
+      { selector: 'rpc-port', name: 'port' }
+    ].map(assertRpc.bind(client));
+  },
 
-    // Then modify the value
-    // todo [adgo] 20.04.2016 - implement after resolving https://github.com/tomusdrw/eth-node-status-page/issues/26
+  'change extradata' (client) {
+    const submit = el('MiningSettings-extra-data-submit');
+    const cancel = el('MiningSettings-extra-data-cancel');
+    const edit = el('MiningSettings-extra-data-edit');
+    const reset = el('MiningSettings-extra-data-reset');
+    const input = el('MiningSettings-extra-data', 'input');
+    const val = mockedResponses.rpc.find(method => method.name === 'ethcore_extraData').formattedResponse;
+    const newVal = 'Parity//213.beta/awesometesty/ubuntu-29';
+    // submit/cancel are hidden on load, edit and reset button displayed but not visible
+    client.expect.element(submit).to.not.be.present;
+    client.expect.element(cancel).to.not.be.present;
+    client.expect.element(edit).to.be.present;
+    client.expect.element(edit).to.not.be.visible;
+    client.expect.element(reset).to.be.present;
+    client.expect.element(reset).to.not.be.visible;
+    // display edit and reset button (sleep for transition delay)
+    client.moveToElement(input, 0, 0).pause(220);
+    client.expect.element(edit).to.be.visible;
+    client.expect.element(reset).to.be.visible;
+    // switch to edit mode
+    client.click(edit);
+    // submit/cancel displayed, edit button hidden
+    client.expect.element(submit).to.be.present;
+    client.expect.element(cancel).to.be.present;
+    client.expect.element(edit).to.not.be.present;
+    client.expect.element(reset).to.not.be.present;
+    // modify the value and cancel, expect initial value
+    client.clearValue(input);
+    client.setValue(input, newVal);
+    client.click(cancel);
+    client.expect.element(input).to.have.value.that.equals(val);
+    client.expect.element(submit).to.not.be.present;
+    client.expect.element(cancel).to.not.be.present;
+    client.expect.element(edit).to.be.present;
+    client.expect.element(reset).to.be.present;
+    // modify the value and click outside, expect new value, action buttons present
+    client.moveToElement(input, 0, 0).pause(220);
+    client.click(edit);
+    client.clearValue(input);
+    client.setValue(input, newVal);
+    client.click(el('Status-mining')); // outside click
+    client.expect.element(input).to.have.value.that.equals(newVal);
+    client.expect.element(submit).to.be.present;
+    client.expect.element(cancel).to.be.present;
+    client.expect.element(edit).to.not.be.present;
+    client.expect.element(reset).to.not.be.present;
+    // submit, expect new value, edit button hidden
+    client.clearValue(input);
+    client.setValue(input, newVal);
+    client.click(submit);
+    client.expect.element(input).to.have.value.that.equals(newVal);
+    client.expect.element(submit).to.not.be.present;
+    client.expect.element(cancel).to.not.be.present;
+    client.expect.element(edit).to.be.present;
+    client.expect.element(reset).to.be.present;
   },
 
   after (client) {
@@ -94,4 +140,13 @@ function assertNetwork (method) {
 function assertNav (link) {
   const selector = `Header-${link}-link`;
   this.expect.element(el(selector)).to.be.present;
+}
+
+function assertRpc (method) {
+  const mockedMethod = mockedResponses.rpc.find(m => m.name === 'ethcore_rpcSettings');
+  let expected = mockedMethod.response[method.name];
+  if (method.name === 'enabled') {
+    expected = expected ? 'yes' : 'no';
+  }
+  this.expect.element(el(`Status-${method.selector}`, 'input')).to.have.value.that.contain(expected);
 }
