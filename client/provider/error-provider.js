8454ac4df3f80b7d335d1f7a9011232de5ba4b89
Adam Goldman <adamgoldman7@gmail.com>
2016-04-29 20:08:59 +0300

    add tests to error provider + refactor

--- client/provider/error-provider.js
+++ client/provider/error-provider.js
@@ -1,26 +1,22 @@
 
 import { addErrorToast } from '../actions/toastr';
 
-// param: xs => [{}]
 export function hasErrors (xs) {
   if (!xs) {
     return;
   }
-
   return !!xs.find(isError);
 }
 
-export function isError (obj) {
-  return Object.prototype.toString.call(obj) === '[object Error]';
-}
-
-// param: xs => [{}]
-export function toastErrors (xs, dispatch) {
-  xs.filter(isError)
-     .map(obj => addErrorToast(obj.toString()))
-     .map(dispatch);
+export function isError (x) {
+  return Object.prototype.toString.call(x) === '[object Error]';
 }
 
 export function toastError (obj, dispatch) {
   dispatch(addErrorToast(obj.toString()));
+}
+
+export function toastErrors (xs, dispatch) {
+  xs.filter(isError)
+     .map(obj => toastError(obj, dispatch));
 }
