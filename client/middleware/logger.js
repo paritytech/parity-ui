3383ac58a4352a9072c5d7c4d880175749096f4b
Adam Goldman <adamgoldman7@gmail.com>
2016-04-29 12:55:23 +0300

    remove formatting from logger middlewware

--- client/middleware/logger.js
+++ client/middleware/logger.js
@@ -1,7 +1,7 @@
 
 export default store => next => action => {
   if (store.getState().logger.logging) {
-    const msg = [`[${now()}] action: `, action.type, 'payload: ', formatPayload(action.payload)];
+    const msg = [`[${now()}] action: `, action.type, 'payload: ', action.payload];
     const logMethod = action.type.indexOf('error') > -1 ? 'error' : 'log';
     console[logMethod](...msg);
   }
@@ -16,9 +16,3 @@ function now () {
   return `${hour}::${minutes}::${seconds}`;
 }
 
-export function formatPayload (payload) {
-  if (typeof payload === 'string' || typeof payload === 'number' || typeof payload === 'boolean') {
-    return payload;
-  }
-  return JSON.stringify(payload);
-}
--- client/middleware/logger.spec.js
+++ client/middleware/logger.spec.js
@@ -1,7 +1,7 @@
 /* global describe, it, beforeEach, afterEach, expect */
 
 import sinon from 'sinon';
-import logger, { formatPayload } from './logger';
+import logger from './logger';
 
 describe('MIDDLEWARE: LOGGER', () => {
   describe('MIDDLEWARE', () => {
@@ -49,56 +49,6 @@ describe('MIDDLEWARE: LOGGER', () => {
       // then
       expect(console.log.called).to.be.false;
       expect(console.error.calledOnce).to.be.true;
-    });
-  });
-
-  describe('FORMAT', () => {
-    it('should return number when number is passed', () => {
-      // given
-      const payload = 123;
-
-      // when
-      const res = formatPayload(payload);
-
-      // then
-      expect(res).to.equal(payload);
-    });
-
-    it('should return boolean when boolean is passed', () => {
-      // given
-      const payload = true;
-
-      // when
-      const res = formatPayload(payload);
-
-      // then
-      expect(res).to.equal(payload);
-    });
-
-    // [adgo] 30.04.2016 - spying on JSON.stringify didn't work
-    // so I'm manually testing the values returned
-    it('should return stringified array when array is passed', () => {
-      // given
-      const payload = ['foo', true];
-
-      // when
-      const res = formatPayload(payload);
-
-      // then
-      expect(res).to.equal('["foo",true]');
-    });
-
-    // [adgo] 30.04.2016 - spying on JSON.stringify didn't work
-    // so I'm manually testing the values returned
-    it('should return stringified array when array is passed', () => {
-      // given
-      const payload = {foo: true, bar: 'not so true'};
-
-      // when
-      const res = formatPayload(payload);
-
-      // then
-      expect(res).to.equal('{"foo":true,"bar":"not so true"}');
     });
   });
 });
