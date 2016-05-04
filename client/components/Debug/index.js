fb05627af521b17ecd3318c772d2f204db4a42bb
adamgold <adamgoldman7@gmail.com>
2016-05-04 15:10:08 +0300

    enhance dev logs

--- client/components/Debug/index.js
+++ client/components/Debug/index.js
@@ -5,21 +5,14 @@ import style from './style.css';
 
 export default class Debug extends Component {
 
-  renderLogs () {
-    return this.props.debug.logs.slice().reverse().map(log => (
-      <pre className={style.log} key={log}>
-        {log}
-      </pre>
-    ));
-  }
-
   render () {
     return (
       <div className='dapp-flex-content'>
         <main className={`dapp-content ${style.container}`}>
           <div className={'dapp-container'}>
             <h1><span>Debugging</span> logs</h1>
-            <h2>{this.props.debug.levels || '-'}</h2>
+            {this.renderActions()}
+            <h2 className={style.subheader}>{this.props.debug.levels || '-'}</h2>
             <div className={style.logs}>
               {this.renderLogs()}
             </div>
@@ -29,11 +22,42 @@ export default class Debug extends Component {
     );
   }
 
+  renderLogs () {
+    return this.props.debug.logs.slice().reverse().map((log, idx) => (
+      <pre className={style.log} key={idx}>
+        {log}
+      </pre>
+    ));
+  }
+
+  renderActions () {
+    const toggleClass = this.props.debug.logging ? 'icon-control-pause' : 'icon-control-play';
+    return (
+      <div className={style.actions}>
+        <a><i onClick={::this.toggle} className={toggleClass}></i></a>
+        <a><i onClick={::this.clear} className='icon-trash'></i></a>
+      </div>
+    );
+  }
+
+  clear () {
+    this.props.actions.removeDevLogs();
+  }
+
+  toggle () {
+    this.props.actions.updateDevLogging(!this.props.debug.logging);
+  }
+
 }
 
 Debug.propTypes = {
+  actions: PropTypes.shape({
+    removeDevLogs: PropTypes.func.isRequired,
+    updateDevLogging: PropTypes.func.isRequired
+  }).isRequired,
   debug: PropTypes.shape({
     levels: PropTypes.string.isRequired,
+    logging: PropTypes.bool.isRequired,
     logs: PropTypes.arrayOf(PropTypes.string).isRequired
   }).isRequired,
   status: PropTypes.shape({
--- client/reducers/debug.js
+++ client/reducers/debug.js
@@ -3,8 +3,11 @@ import { handleActions } from 'redux-actions';
 
 const initialState = {
   levels: '',
+  logging: true,
   logs: []
 };
+
+const maxLogs = 1024;
 
 export const actionHandlers = {
 
@@ -15,10 +18,28 @@ export const actionHandlers = {
     };
   },
 
-  'update devLogs' (state, action) {
+  'remove devLogs' (state, action) {
     return {
       ...state,
-      logs: action.payload
+      logs: []
+    };
+  },
+
+  'update devLogging' (state, action) {
+    return {
+      ...state,
+      logging: action.payload
+    };
+  },
+
+  'update devLogs' (state, action) {
+    if (!state.logging) {
+      return { ...state };
+    }
+
+    return {
+      ...state,
+      logs: action.payload.concat(state.logs).slice(0, maxLogs)
     };
   }
 
