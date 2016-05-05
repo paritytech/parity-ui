1c2ff3e42ad657d040bb5b14ad4b67233fdc4e24
Tomusdrw <tomusdrw@gmail.com>
2016-05-05 17:41:57 +0200

    Some refactoring of web3 forwarder

--- client/components/DappContent/DappContent.js
+++ client/components/DappContent/DappContent.js
@@ -2,46 +2,13 @@ import React from 'react';
 import styles from './styles.css';
 
 import {Web3Component} from '../Web3Component/Web3Component';
+import {Web3Forwarder} from './Web3Forwarder';
 
 export class DappContent extends Web3Component {
 
   constructor (...args) {
     super(...args);
-
-    window.addEventListener('message', (ev) => {
-      const {origin, data, source} = ev;
-      const {type, payload} = data;
-      function reply (res) {
-        source.postMessage(res, origin);
-      }
-
-      if (type === 'web3_sendAsync') {
-        if (payload.method === 'eth_accounts') {
-          delete payload.method;
-          payload.result = this.props.accounts;
-          reply({
-            type,
-            err: null,
-            response: payload,
-            id: payload.id
-          });
-          return;
-        }
-
-        this.provider().sendAsync(payload, (err, response) => {
-          reply({
-            type, err, response,
-            id: payload.id
-          });
-        });
-        return;
-      }
-      console.warn('Did not understood:', data);
-    });
-  }
-
-  provider () {
-    return this.context.web3.currentProvider;
+    this.forwarder = new Web3Forwarder(this);
   }
 
   render () {
