d4786073f2ad8fc42a67a40e9ebd2428031c2f72
Tomusdrw <tomusdrw@gmail.com>
2016-05-05 18:56:45 +0200

    Updating token

---
+++ client/components/DappContent/Web3Forwarder.js
@@ -0,0 +1,46 @@
+export class Web3Forwarder {
+  constructor (component) {
+    this.component = component;
+    this.addListener();
+  }
+
+  provider () {
+    this.component.context.web3.currentProvider;
+  }
+
+  handleMethod (method, payload, cb) {
+    if (method === 'eth_accounts') {
+      const response = {
+        id: payload.id,
+        jsonrpc: payload.jsonrpc,
+        result: this.component.props.accounts
+      };
+      return cb(null, response);
+    }
+
+    this.provider().sendAsync(payload, cb);
+  }
+
+  messageListener (ev) {
+    const {origin, data, source} = ev;
+    const {type, payload} = data;
+
+    function reply (res) {
+      source.postMessage(res, origin);
+    }
+
+    if (type === 'web3_sendAsync') {
+      this.handleMethod(payload.method, payload, (err, response) => {
+        reply({
+          type, err, response, id: response.id
+        });
+      });
+      return;
+    }
+    console.warn('Did not understood:', data);
+  }
+
+  addListener () {
+    window.addEventListener('message', ::this.messageListener);
+  }
+}
