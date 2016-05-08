e8428caba1bb79c0b056e9e0de0faff05ccff1ac
Tomusdrw <tomusdrw@gmail.com>
2016-05-08 20:49:17 +0200

    Fixing e2e tests

--- client/components/MiningSettings/decodeExtraData.spec.js
+++ client/components/MiningSettings/decodeExtraData.spec.js
@@ -1,9 +1,8 @@
-/* global describe, it, xit, beforeEach, expect */
+/* global describe, it, expect */
 
 import {decodeExtraData} from './decodeExtraData';
 
 describe('MINING SETTINGS', () => {
-
   describe('EXTRA DATA', () => {
     const str = 'parity/1.0.0/1.0.0-beta2';
     const encoded = '0xd783010000867061726974798b312e302e302d6265746132';
@@ -12,5 +11,4 @@ describe('MINING SETTINGS', () => {
       expect(decodeExtraData(encoded)).to.equal(str);
     });
   });
-
 });
