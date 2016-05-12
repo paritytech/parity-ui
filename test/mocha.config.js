b7b527c1275a36236c4e3b1e8e76583d5e028375
Jaco Greeff <jacogr@gmail.com>
2016-05-12 16:16:51 +0200

    setup jsdom for React component tests

--- test/mocha.config.js
+++ test/mocha.config.js
@@ -1,6 +1,15 @@
 import chai from 'chai';
-import 'mock-local-storage';
+import jsdom from 'jsdom';
+
 // expose expect to global so we won't have to manually import & define it in every test
 global.expect = chai.expect;
+
+// setup jsdom
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = global.window.navigator;
+
+// only imported after jsdom setup
+import 'mock-local-storage';
 
 module.exports = {};
