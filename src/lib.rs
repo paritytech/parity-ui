f7babde861fbce52c5f7d624b115e8413824aec6
Tomusdrw <tomusdrw@gmail.com>
2016-05-18 11:35:47 +0200

    HomePage

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ impl WebApp for App {
   fn info(&self) -> Info {
     Info {
       name: "Identity Manager".to_owned(),
-      version: "0.2.0".to_owned(),
+      version: "0.2.2".to_owned(),
       author: "Ethcore <admin@ethcore.io>".to_owned(),
       description: "Injectable identity manager for Parity".to_owned(),
       icon_url: "icon.png".to_owned(),
@@ -29,6 +29,8 @@ impl Default for App {
   fn default() -> Self {
     let files = {
       let mut files = HashMap::new();
+      files.insert("home.js", File { path: "home.js", content_type: "application/javascript", content: include_bytes!("./web/home.js") });
+      files.insert("index.html", File { path: "index.html", content_type: "text/html", content: include_bytes!("./web/index.html") });
       files.insert("inject.js", File { path: "inject.js", content_type: "application/javascript", content: include_bytes!("./web/inject.js") });
       files
     };
