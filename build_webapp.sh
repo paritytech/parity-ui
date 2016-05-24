66a6f7f92bebb90bb89549c31639dc4a57932423
Tomusdrw <tomusdrw@gmail.com>
2016-05-24 19:22:53 +0200

    Removing generator step

--- build_webapp.sh
+++ build_webapp.sh
@@ -5,7 +5,6 @@ set -e
 # This script assumes following directory structure:
 # - parity-idmanager-rs
 # - parity-web-ui ($ git clone https://github.com/ethcore/parity-web-ui.git)
-# - parity-webapp ($ git clone https://github.com/ethcore/parity-webapp.git)
 
 cd ../parity-web-ui
 rm static -R || true
@@ -21,5 +20,3 @@ cp ${UI}/home.js .
 cp ${UI}/home.html index.html
 cp ${UI}/home/cols.frame.html .
 cp ${UI}/home/cols.frame.js .
-FULL_NAME="Identity Manager" ../../../parity-webapp/generate/index.js > ../lib.rs
-
