8ebe731319f1f8bfb1dd2e2742b54b1a51e6f3b5
Tomusdrw <tomusdrw@gmail.com>
2016-05-24 20:26:56 +0200

    Code generation

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ cd -
 
 ./build_lib.sh
 
-git checkout master -- src/web/web3.js
 echo "Manualy modify ./src/web/index.html and include changes from old index.html"
 git diff master -- src/web/index.html
 echo "Bump version"
--- build_lib.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-set -e
-
-cd src/web
-FULL_NAME="Ethereum Wallet" ../../../parity-webapp/generate/index.js > ../lib.rs
-
