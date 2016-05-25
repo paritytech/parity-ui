f4e096875a97e8cca3fe7c65ec0e22166a448a11
Jaco Greeff <jacogr@gmail.com>
2016-05-25 18:44:12 +0200

    test actions & utility functions

--- client/components/CallsToolbar/CallsToolbar.spec.js
+++ client/components/CallsToolbar/CallsToolbar.spec.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import sinon from 'sinon';
 import { shallow } from 'enzyme';
 
 import '../../env-specific/tests';
@@ -47,6 +48,55 @@ describe('components/CallsToolbar', () => {
 
     it('renders the Copy button', () => {
       expect(btncontainer).to.have.descendants('[data-test="CallsToolbar-copyCallToClipboard"]');
+    });
+  });
+
+  describe('actions', () => {
+    const call = { callNo: 456, name: 'eth_call', params: '', response: '' };
+    const actions = { fireRpc: sinon.stub(), copyToClipboard: sinon.stub(), selectRpcMethod: sinon.stub() };
+
+    let rendered;
+    let instance;
+
+    before(() => {
+      rendered = shallow(<CallsToolbar call={call} callEl={callEl} containerEl={containerEl} actions={actions} />);
+      instance = rendered.instance();
+    });
+
+    it('calls copyToClipboard with action copyToClipboard', () => {
+      instance.copyToClipboard();
+      expect(actions.copyToClipboard).to.be.calledOnce;
+    });
+
+    it('calls setCall with action selectRpcMethod', () => {
+      instance.setCall();
+      expect(actions.selectRpcMethod).to.be.calledOnce;
+    });
+
+    it('calls makeCall with action fireRpc', () => {
+      instance.makeCall();
+      expect(actions.fireRpc).to.be.calledOnce;
+    });
+  });
+
+  describe('utility', () => {
+    const call = { callNo: 456, name: 'eth_call', params: '', response: '' };
+    let rendered;
+    let instance;
+
+    before(() => {
+      rendered = shallow(<CallsToolbar call={call} callEl={callEl} containerEl={containerEl} />);
+      instance = rendered.instance();
+    });
+
+    describe('.hasScrollbar', () => {
+      it('correctly returns true when scrollbar', () => {
+        expect(instance.hasScrollbar({ clientHeight: 123, scrollHeight: 456 })).to.be.true;
+      });
+
+      it('correctly returns false when no scrollbar', () => {
+        expect(instance.hasScrollbar({ clientHeight: 456, scrollHeight: 123 })).to.be.false;
+      });
     });
   });
 });
