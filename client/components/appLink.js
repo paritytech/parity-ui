ee94eabd20e6cf8736b2f25b810349393cbf40c3
goldylucks <adamgoldman7@gmail.com>
2016-05-25 11:27:43 +0200

    DRY applink

--- client/components/appLink.js
+++ client/components/appLink.js
@@ -1,16 +1,21 @@
 
 const SUBDOMAIN = '.parity';
+const CURRENT_APP_NAME = getAppFromWindow();
 
 export function appLink (appId) {
   if (isUsingSubdomains()) {
-    return `//${appId}${SUBDOMAIN}/`;
+    return appSubdomainLink(appId);
   }
 
   return `/${appId}/`;
 }
 
+function appSubdomainLink (appId) {
+  return `${window.location.protocol}//${appId}${SUBDOMAIN}/`;
+}
+
 export function appPrettyLink () {
-  return window.location.protocol + '://' + getAppFromWindow() + SUBDOMAIN;
+  return appSubdomainLink(CURRENT_APP_NAME);
 }
 
 function getAppFromWindow () {
