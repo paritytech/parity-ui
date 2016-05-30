0b894b70e34718aebfb43d2ed5313d0c3a80d329
Tomusdrw <tomusdrw@gmail.com>
2016-05-30 20:42:05 +0200

    Fixing e2e test

--- test/e2e/rpc-page.js
+++ test/e2e/rpc-page.js
@@ -56,7 +56,7 @@ module.exports = {
     // // assert response
     client.waitForElementVisible(el('Call-call-1'), 1000, false);
     client.expect.element(el('Call-call-1', '> span:nth-child(1)')).text.to.contain('#1');
-    client.expect.element(el('Call-call-1', '> pre:nth-of-type(1)')).text.to.contain(`${method}(${valueToSet})`);
+    client.expect.element(el('Call-call-1', '> pre:nth-of-type(1)')).text.to.contain(`${method}("${valueToSet}")`);
     client.expect.element(el('Call-call-1', '> pre:nth-of-type(2)')).text.to.contain(mckResponse);
   },
 
