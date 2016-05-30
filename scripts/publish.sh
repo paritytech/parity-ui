7fd1cef2b2b1a7b5cb5533d0e370ee40cb2dd1e9
goldylucks <adamgoldman7@gmail.com>
2016-05-30 20:22:04 +0200

    v0.0.36

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,7 +9,10 @@ SOURCE_BRANCH="master"
 # 2. Not source branch
 # 3. Not pushing GH tags
 if [[ ("$TRAVIS_PULL_REQUEST" != "false") || ("$TRAVIS_BRANCH" != "$SOURCE_BRANCH") || ( -z ${TRAVIS_TAG+x} ) ]]; then
-    echo 'Not deploying';
+    echo 'Not deploying because:';
+    if [[ ("$TRAVIS_PULL_REQUEST" != "false") ]]; then echo 'its a pull request'; fi
+    if [[ ("$TRAVIS_BRANCH" != "$SOURCE_BRANCH") ]]; then echo 'not src branch'; fi
+    if [[ ( -z ${TRAVIS_TAG+x} ) ]]; then echo 'not tags'; fi
     exit 0
 fi
 
@@ -18,11 +21,11 @@ PUBLISH_PATH="build"
 # publish npm package
 rm -rf "${PUBLISH_PATH}"
 mkdir "${PUBLISH_PATH}"
-cp -r src/* "${PUBLISH_PATH}"/
-cp LICENSE "${PUBLISH_PATH}"/
-cp README.md "${PUBLISH_PATH}"/
-cp package.json "${PUBLISH_PATH}"/
-cd "${PUBLISH_PATH}"/
+cp -r src/* "${PUBLISH_PATH}/"
+cp LICENSE "${PUBLISH_PATH}/"
+cp README.md "${PUBLISH_PATH}/"
+cp package.json "${PUBLISH_PATH}/"
+cd "${PUBLISH_PATH}/"
 npm publish
 
 exit 0
