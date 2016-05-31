739dbb0dccc22e2e62a86cfafc53052e4003ffba
goldylucks <adamgoldman7@gmail.com>
2016-05-31 17:34:33 +0200

    v0.0.44

---
+++ scripts/local-publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+"${DIR}/pre-publish.sh"
+npm publish
+rm -f "${DIR}/../index.js"
+
+exit 0
--- scripts/pre-publish.sh
+++ scripts/pre-publish.sh
@@ -2,6 +2,11 @@
 # See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
 set -o errexit
 
-mv src/* .
+BUILD_PATH="build"
+
+rm -rf "$BUILD_PATH"
+npm run build
+
+cp "${BUILD_PATH}/index.js" index.js
 
 exit 0
