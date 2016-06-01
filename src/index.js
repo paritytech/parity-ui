d1adb857cdd16128787824829f7e466da0779b6e
goldylucks <adamgoldman7@gmail.com>
2016-06-01 19:26:21 +0200

    export web3 WS provider

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ export Identicon from './Identicon';
 export Transaction from './Transaction';
 export Web3Component from './Web3Component';
 export Web3Provider from './Web3Provider';
+export Web3WebSocketProvider from '.util/Web3WebSocketProvider';
 export RpcAutoComplete from './RpcAutoComplete';
