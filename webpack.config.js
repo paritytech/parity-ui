3e7f488127f62798b74a3f57174a05ddf23570d7
Tomusdrw <tomusdrw@gmail.com>
2016-06-08 00:16:55 +0200

    Changing loader

--- webpack.config.js
+++ webpack.config.js
@@ -62,7 +62,7 @@ module.exports = {
       },
       {
         test: /\.(woff(2)|ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
+        loader: 'url-loader?limit=10000'
       }
     ],
     noParse: [
