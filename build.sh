bab787881149fa30cb910f6cab3aa22282cf60ec
Tomusdrw <tomusdrw@gmail.com>
2016-06-08 14:42:29 +0200

    Displaying all-accounts

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ cd meteor-dapp-wallet/app
 meteor-build-client ../../src/web --path "/wallet/"
 cd -
 
-sed -i '5i\<script src="/parity-utils/inject.js"></script>' ./src/web/index.html
+sed -i '5i\<script src="/parity-utils/inject.js" all-accounts></script>' ./src/web/index.html
 sed -i '7i\<script>delete window.require;__meteor_runtime_config__.meteorEnv={NODE_ENV: "production"};</script>' ./src/web/index.html
 echo "Bump version"
 vim Cargo.toml
