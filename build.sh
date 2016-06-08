2e6e94150c22ef348af7f3169aea41e591326e98
Tomusdrw <tomusdrw@gmail.com>
2016-06-08 15:14:30 +0200

    Restoring icon

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf ./src/web/*
 cd meteor-dapp-wallet/app
 meteor-build-client ../../src/web --path "/wallet/"
 cd -
-
+cp icon.png ./src/web/
 sed -i '5i\<script src="/parity-utils/inject.js" all-accounts></script>' ./src/web/index.html
 sed -i '7i\<script>delete window.require;__meteor_runtime_config__.meteorEnv={NODE_ENV: "production"};</script>' ./src/web/index.html
 echo "Bump version"
