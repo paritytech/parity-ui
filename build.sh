b02f50a0c07c1f2c3f02ca1a50923d8c22f25a47
Tomusdrw <tomusdrw@gmail.com>
2016-06-08 14:34:33 +0200

    Bump

--- build.sh
+++ build.sh
@@ -7,9 +7,7 @@ cd meteor-dapp-wallet/app
 meteor-build-client ../../src/web --path "/wallet/"
 cd -
 
-./build_lib.sh
-
-echo "Manualy modify ./src/web/index.html and include changes from old index.html"
-git diff master -- src/web/index.html
+sed -i '5i\<script src="/parity-utils/inject.js"></script>' ./src/web/index.html
+sed -i '7i\<script>delete window.require;__meteor_runtime_config__.meteorEnv={NODE_ENV: "production"};</script>' ./src/web/index.html
 echo "Bump version"
 vim Cargo.toml
