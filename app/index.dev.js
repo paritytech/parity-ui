a449014928f38fd071fb7d461d8559969c96937f
goldylucks <adamgoldman7@gmail.com>
2016-06-11 20:57:10 +0200

    add initial state support

--- app/index.dev.js
+++ app/index.dev.js
@@ -5,7 +5,7 @@ import app from './';
 const emitter = ee({});
 
 tokenGetter(initToken => {
-  app(initToken, tokenSetter, addTokenListener);
+  app(initToken, tokenSetter, addTokenListener, initialState());
 });
 
 function tokenGetter (cb) {
@@ -21,4 +21,30 @@ function tokenSetter (token, cb) {
 
 function addTokenListener (cb) {
   emitter.on('sysuiToken', cb);
+}
+
+function initialState () {
+  return {
+    transactions: {
+      finished: [
+        {
+          id: '0x01',
+          from: '0xB4C2328115Fb5A2D7503Cb11969A8f25f3456638',
+          value: 87456893245,
+          fee: 342532,
+          txHash: '0xlKJHf4KLFHdfg',
+          data: 'bla bla foo bar!',
+          to: '0x575B71fa5dc1d8a23a242F70a70ae88d22B96107'
+        },
+        {
+          id: '0x02',
+          from: '0xB4C2328115Fb5A2D7503Cb11969A8f25f3456638',
+          value: 87456893245,
+          fee: 342532,
+          to: '0x575B71fa5dc1d8a23a242F70a70ae88d22B96107'
+        }
+      ],
+      pending: []
+    }
+  }
 }
--- app/index.js
+++ app/index.js
@@ -16,11 +16,11 @@ import middlewares from './middlewares';
 import createStore from './store/configureStore';
 import Routes from './routes';
 
-export default function app (initToken, tokenSetter, addTokenListener) {
+export default function app (initToken, tokenSetter, addTokenListener, initialState) {
   const web3WebSocketProvider = new Web3WebSocketProvider(initToken, addTokenListener);
   const web3 = new Web3(web3WebSocketProvider);
 
-  const store = createStore(middlewares(tokenSetter));
+  const store = createStore(middlewares(tokenSetter), initialState);
   ReactDOM.render(
     <Provider store={ store }>
       <Web3Provider web3={ web3 }>
