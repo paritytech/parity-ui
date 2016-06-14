8f22f9dc5a76a9ad83a1f6d064f4c2a455806ea8
goldylucks <adamgoldman7@gmail.com>
2016-06-14 20:43:57 +0200

    remove call to injectTapEventPlugin

--- src/MuiThemeProvider/MuiThemeProvider.js
+++ src/MuiThemeProvider/MuiThemeProvider.js
@@ -1,9 +1,5 @@
 
 import React, { Component, PropTypes } from 'react';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-// Needed for onTouchTap, for material ui
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
 
 import { deepOrange500 } from 'material-ui/styles/colors';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
