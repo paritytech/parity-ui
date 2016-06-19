67ec0ad078c0978d615fcf5c277d97e40932068a
goldylucks <adamgoldman7@gmail.com>
2016-06-19 22:50:08 +0200

    extract WS WIP

--- chrome/extension/background/ws.js
+++ chrome/extension/background/ws.js
@@ -1,48 +1,30 @@
 /* global chrome */
 import isEqual from 'lodash.isequal';
-import { keccak_256 } from 'js-sha3';
 import logger from '../../utils/logger';
 import { SIGNER_META_TITLE, POPUP_URL } from '../../constants/constants';
 import { weiHexToEthString } from '../../utils/formatters';
+import Ws from '../../utils/ws';
 
-class Ws {
+class BgWs {
 
   constructor () {
-    this.id = 1;
-    this.callbacks = {};
-    this.queue = []; // hold calls until ws is connected on init or if disconnected
-    this.isConnected = false;
+    this.ws = new Ws({
+      openCb: this.pollTransactions, 
+      errorCb: this.reset
+    });
     chrome.storage.onChanged.addListener(this.onSysuiTokenChange);
     chrome.notifications.onClicked.addListener(this.onNotificationclick);
     chrome.browserAction.setBadgeBackgroundColor({ color: '#f00'});
     this.reset();
-    this.init();
   }
 
-  init () {
-    chrome.storage.local.get('sysuiToken', obj => {
-      let { sysuiToken } = obj;
-
-      if (!sysuiToken) {
-        return logger.log('[BG WS] no sysuiToken in LS');
-      }
-
-      sysuiToken = JSON.parse(sysuiToken)
-      const hash = this.hash(sysuiToken);
-
-      try {
-        this.ws = new WebSocket('ws://127.0.0.1:8180', hash);
-      } catch (err) {
-        logger.warn('cant connect to ws ', err);
-      }
-      this.ws.addEventListener('error', this.onWsError);
-      this.ws.addEventListener('open', this.onWsOpen);
-    });
+  init (token) {
+    this.ws.init(token);
   }
 
-  reset () {
-    // todo [adgo]- remove transactions from LS
-    chrome.storage.local.set({ pendingTransactions: JSON.stringify([]) });
+  reset = () => {
+    this.transactions = [];
+    this.setBadgeText(0);
   }
 
   // when token changes in chrome LS
@@ -52,100 +34,30 @@ class Ws {
     }
     const newSysuiToken = JSON.parse(changes.sysuiToken.newValue);
     logger.log('[BG WS] sysuiToken changed! ', newSysuiToken);
-    this.init();
+    this.init(newSysuiToken);
   }
 
-  // todo [adgo] - add comment: link to explanation
-  hash (sysuiToken) {
-    const time = parseInt(new Date().getTime() / 1000, 10);
-    return keccak_256(sysuiToken + ':' + time) + '_' + time;
-  }
-
-  onWsOpen = () => {
-    logger.log('[BG WS] connected');
-    this.isConnected = true;
-    this.executeQueue();
-    chrome.storage.local.set({ pendingTransactions: JSON.stringify([]) });
-    this.ws.addEventListener('close', this.onWsClose);
-    this.ws.addEventListener('message', this.onWsMsg);
-    this.fetchTransactions();
-  }
-
-  onWsError = (err) => {
-    logger.warn('[BG WS] error ', err);
-    this.reset();
-    setTimeout(() => this.init(), 5000);
-  }
-
-  onWsClose = () => {
-    logger.warn('[BG WS] closed!');
-    this.errorOutCallbacks();
-    this.isConnected = false;
-    this.reset();
-    this.init();
-  }
-
-  onWsMsg = msg => {
-    try {
-      msg = JSON.parse(msg.data);
-    } catch (err) {
-      return logger.warn('[BG WS] unknown msg from server: ', msg, err);
-    }
-    const cb = this.callbacks[msg.id];
-    delete this.callbacks[msg.id];
-    if (!cb) {
-      // Ignoring - no one is waiting for that response.
-      return;
-    }
-    // callback otherwise
-    cb(msg.result);
-  }
-
-  timeoutFetchTransactions = () => {
-    setTimeout(::this.fetchTransactions, 2000);
-  }
-
-  fetchTransactions () {
+  pollTransactions = () => {
     this.send('personal_transactionsToConfirm', [], txsWs => {
-      this.setBadgeText(txsWs.length)
-      chrome.storage.local.get('pendingTransactions', obj => {
-        try {
-          const transactionsLs = JSON.parse(obj.pendingTransactions);
-          if (isEqual(txsWs, transactionsLs)) {
-            return;
-          }
-          logger.log('[BG WS] transactions changed');
-          logger.log('[BG WS] previous (LS): ', transactionsLs);
-          logger.log('[BG WS] current (WS): ', txsWs);
-
-          if (txsWs.length > transactionsLs.length) {
-            this.createNotification(txsWs);
-          }
-
-          chrome.storage.local.set({ pendingTransactions: JSON.stringify(txsWs) });
-        } catch (err) {
-          logger.warn('[BG WS] bad data from extension local storage! object should contain transactions ', obj, err);
-        } finally {
-          this.timeoutFetchTransactions()
-        }
-      });
+      if (isEqual(txsWs, this.transactions)) {
+        return;
+      }
+      logger.log('[BG WS] transactions changed', txsWs);
+      this.setBadgeText(txsWs.length);
+      if (txsWs.length > transactionsLs.length) {
+        this.createNotification(txsWs);
+      }
+      this.transactions = txsWs;
+      setTimeout(() => this.pollTransactions, 2000);
     });
   }
 
   send (method, params, callback) {
-    if (!this.isConnected) {
-      this.queue.push({ method, params, callback });
-      logger.log('[BG WS] incoming msg when not connected, adding to queue');
-      return;
-    }
-    const id = this.id;
-    this.id++;
-    const payload = JSON.stringify({
+    const payload = {
       jsonrpc: '2.0',
-      id, method, params
-    });
-    this.callbacks[id] = callback;
-    this.ws.send(payload);
+      method, params
+    };
+    this.ws.send(payload, callback);
   }
 
   setBadgeText (text) {
@@ -154,22 +66,6 @@ class Ws {
       text = '';
     }
     chrome.browserAction.setBadgeText({ text });
-  }
-
-  executeQueue () {
-    logger.log('[BG WS] executing queue: ', this.queue);
-    this.queue.forEach(call => {
-      this.send(call.method, call.params, call.callback);
-    });
-    this.queue = [];
-  }
-
-  errorOutCallbacks () {
-    logger.log('[BG WS] erroring out callbacks: ', this.callbacks);
-    for (const msgId in this.callbacks) {
-      callbacks[msgId]('WS disconnected, cb cannot be called');
-    }
-    this.callbacks = {};
   }
 
   // todo [adgo] - handle multiple new pending transactions
@@ -217,4 +113,13 @@ class Ws {
 
 }
 
-new Ws();
+const bgWs = new BgWs();
+
+chrome.storage.local.get('sysuiToken', obj => {
+  let { sysuiToken } = obj;
+  if (!sysuiToken) {
+    return logger.log('[BG WS] no sysuiToken in LS');
+  }
+  sysuiToken = JSON.parse(sysuiToken);
+  bgWs.init(sysuiToken);
+});
---
+++ chrome/utils/ws.js
@@ -0,0 +1,108 @@
+import { keccak_256 } from 'js-sha3';
+import logger from './logger';
+
+export default class Ws {
+
+  constructor (opts) {
+    this.path = opts.path || '127.0.0.1';
+    this.port = opts.port || '8080';
+    this.reconnectTimeout = opts.reconnectTimeout || 5000;
+    this.openCb = opts.openCb || noop;
+    this.errorCb = opts.errorCb || noop;
+    this.closeCb = opts.closeCb || noop;
+    this.isConnected = false;
+    this.callbacks = {};
+    this.queue = [];
+    this.id = 1;
+  }
+
+  init = token => {
+    clearTimeout(this.initTimeout);
+    try {
+      const hash = this.hash(token);
+      this.ws = new WebSocket(`ws://${this.path}:${this.port}`, hash);
+    } catch (err) {
+      logger.warn('[WS] error connecting to ws', err);
+    }
+
+    this.ws.addEventListener('open', this.onOpen);
+    this.ws.addEventListener('error', this.onError);
+  }
+
+  onOpen = () => {
+    logger.log('[WS] connected');
+    this.ws.addEventListener('close', this.onClose);
+    this.ws.addEventListener('message', this.onMsg);
+    this.isConnected = true;
+    this.executeQueue();
+    this.openCb();
+  }
+
+  onClose = () => {
+    logger.warn('[WS] closed');
+    this.executeCbsWithError();
+    this.isConnected = false;
+    this.openCb();
+    this.init(this.token);
+  }
+
+  onError = err => {
+    logger.warn('[WS] error', err);
+    this.closeCb();
+    this.initTimeout = setTimeout(() => this.init(this.token), this.reconnectTimeout);
+  }
+
+  onWsMsg = msg => {
+    try {
+      msg = JSON.parse(msg.data);
+    } catch (err) {
+      return logger.warn('[WS] unknown msg from server: ', msg, err);
+    }
+    const cb = this.callbacks[msg.id];
+    delete this.callbacks[msg.id];
+    if (!cb) {
+      // Ignoring - no one is waiting for that response.
+      return;
+    }
+    // callback otherwise
+    cb(msg.result);
+  }
+
+  send (payload, callback) {
+    if (!this.isConnected) {
+      logger.log('[WS] not connected. incoming msg added to queue');
+      this.queue.push({ payload, callback });
+      return;
+    }
+    const { id } = this;
+    this.id++;
+    payload.id = id;
+    payload = JSON.stringify(payload);
+    this.callbacks[id] = callback;
+    this.ws.send(payload);
+  }
+
+  executeQueue () {
+    logger.log('[WS] executing queue: ', this.queue);
+    this.queue.forEach(call => {
+      this.send(call.payload, call.callback);
+    });
+    this.queue = [];
+  }
+
+  executeCbsWithError () {
+    logger.log('[WS] erroring out callbacks: ', this.callbacks);
+    for (const msgId in this.callbacks) {
+      callbacks[msgId]('[WS] disconnected, cb cannot be called');
+    }
+    this.callbacks = {};
+  }
+
+  hash (token) {
+    const time = parseInt(new Date().getTime() / 1000, 10);
+    return keccak_256(token + ':' + time) + '_' + time;
+  }
+
+}
+
+function noop () {}
