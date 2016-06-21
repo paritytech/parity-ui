4079470b0391947d291a0d6209f6e2ac7fae44f7
goldylucks <adamgoldman7@gmail.com>
2016-06-21 19:07:41 +0200

    convert badge text animator to es7

--- chrome/extension/background/BadgeTextAnimator.js
+++ chrome/extension/background/BadgeTextAnimator.js
@@ -1,100 +1,77 @@
-/* https://github.com/janhancic/chrome-badge-animator/blob/master/src/BadgeAnimator.js */
+export default class BadgeTextAnimator {
 
-/**
- * @class
- * @constructor
- * A Chrome badge (extension button) text animator class.
- * 
- * @param {Object} options Configuration options. Available 
- *  options are: text (the text to display), interval (default: 
- *  500, how fast to animate), repeat (default: true, should the 
- *  text start animating from the beginning when animation 
- *  finishes), size (default: 6, the size of the badge text).
- */
-function BadgeTextAnimator ( options ) {
-  if ( options == null ) {
-    throw new Error( 'You must pass options to the BadgeTextAnimator' );
+  constructor (opts) {
+    if (!opts) {
+      throw new Error('[BTA] requires text in opts');
+    }
+
+    this._opts = {
+      cb: opts.cb || noop,
+      text: opts.text,
+      interval: (opts.interval == null ? 500 : opts.interval),
+      repeat: (opts.repeat == null ? true : opts.repeat),
+      size: (opts.size != null && opts.size > 0 && opts.size <= 6 ? opts.size : 6)
+    };
+
+    this._intervalId = null;
+    this._currentIndex = 0;
   }
 
-  this._options = {
-    cb: options.cb,
-    text: options.text,
-    interval: ( options.interval == null ? 500 : options.interval ),
-    repeat: ( options.repeat == null ? true : options.repeat ),
-    size: ( options.size != null && options.size > 0 && options.size <= 6 ? options.size : 6 )
+  animate () {
+    const spaces = [ '', ' ', '  ', '   ', '    ', '     ', '      ' ];
+    this._setBadgeText(spaces[this._opts.size]);
+    this._doAnimate();
+    this._intervalId = setInterval(this._doAnimate, this._opts.interval);
   };
 
-  this._intervalId = null;
-  this._currentIndex = 0;
-}
+  stop () {
+    clearInterval(this._intervalId);
+    this._intervalId = null;
+    this._opts.cb();
+  };
 
-/**
- * Start the animation of the badge text.
- */
-BadgeTextAnimator.prototype.animate = function () {
-  var spaces = [ '', ' ', '  ', '   ', '    ', '     ', '      ' ];
-  this._setBadgeText( spaces[this._options.size] );
+  _doAnimate = () => {
+    let startAt = this._currentIndex,
+        cutAt = this._opts.size,
+        addBefore = false,
+        chunk,
+        difference;
 
-  this._doAnimate();
+    if (this._currentIndex < this._opts.size) {
+      cutAt = this._currentIndex + 1;
+      addBefore = true;
+      startAt = 0;
+    }
 
-  this._intervalId = setInterval(
-    function () {
-      this._doAnimate();
-    }.bind( this ),
-    this._options.interval
-  );
-};
+    chunk = this._opts.text.substr(startAt, cutAt);
 
-/**
- * Stops the animation.
- */
-BadgeTextAnimator.prototype.stop = function () {
-  clearInterval( this._intervalId );
-  this._intervalId = null;
-  this._options.cb();
-};
-
-/** @private */
-BadgeTextAnimator.prototype._doAnimate = function () {
-  var startAt = this._currentIndex,
-    cutAt = this._options.size,
-    addBefore = false,
-    chunk, difference;
-
-  if ( this._currentIndex < this._options.size ) {
-    cutAt = this._currentIndex + 1;
-    addBefore = true;
-    startAt = 0;
-  }
-
-  chunk = this._options.text.substr( startAt, cutAt );
-
-  if ( chunk.length < this._options.size ) {
-    difference = this._options.size - chunk.length;
-    for ( var i = 0; i <= difference; i++ ) {
-      if ( addBefore === true ) {
-        chunk = ' ' + chunk;
-      } else {
-        chunk = chunk + ' ';
+    if (chunk.length < this._opts.size) {
+      difference = this._opts.size - chunk.length;
+      for (let i = 0; i <= difference; i++) {
+        if (addBefore === true) {
+          chunk = ' ' + chunk;
+        } else {
+          chunk = chunk + ' ';
+        }
       }
     }
-  }
 
-  this._setBadgeText( chunk );
+    this._setBadgeText(chunk);
 
-  this._currentIndex = this._currentIndex + 1;
-  if ( this._currentIndex === this._options.text.length ) {
-    if ( this._options.repeat === true ) {
-      this._currentIndex = 0;
-    } else {
-      this.stop();
+    this._currentIndex = this._currentIndex + 1;
+    if (this._currentIndex === this._opts.text.length) {
+      if (this._opts.repeat === true) {
+        this._currentIndex = 0;
+      } else {
+        this.stop();
+      }
     }
+  };
+
+  _setBadgeText (text) {
+    chrome.browserAction.setBadgeText({ text });
   }
-};
 
-/** @private */
-BadgeTextAnimator.prototype._setBadgeText = function ( text ) {
-  chrome.browserAction.setBadgeText( { text: text } );
-};
+}
 
-export default BadgeTextAnimator;
+function noop () {}
