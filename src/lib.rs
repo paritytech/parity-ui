3bf9e9243a932ac953faa8c12fdd9f274f7646c7
goldylucks <adamgoldman7@gmail.com>
2016-06-22 07:20:26 +0200

    replace with sysuiapp WIP 1

--- src/lib.rs
+++ src/lib.rs
@@ -35,14 +35,7 @@ pub fn handle(resource: &str) -> Option<File> {
     "/" | "/index.html" => file(include_str!("./web/index.html"), "text/html"),
     "/favicon.ico" => file("", "image/ico"),
     "/index.js" => file(include_str!("./web/index.js"), "application/javascript"),
-    "/preact.js" => file(include_str!("./web/preact.js"), "application/javascript"),
-    "/sha3.min.js" => file(include_str!("./web/sha3.min.js"), "application/javascript"),
-    "/milligram.css" => file(include_str!("./web/milligram.css"), "text/css"),
-    "/count.html" => Some(File {
-      content: include_str!("./web/count.html").into(), 
-      mime: "text/html".into(),
-      safe_to_embed: true
-    }),
+    "/app.js" => file(include_str!("./web/app.js"), "application/javascript"),
     _ => None,
   }
 }
