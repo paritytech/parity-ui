592f772530c8821ecead6fe81410612a5e5ddf77
goldylucks <adamgoldman7@gmail.com>
2016-07-05 22:46:05 +0200

    remove console log

--- app/utils/parity.js
+++ app/utils/parity.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import logger from './logger';
 
 export const isParityRunning = path => {
-  console.log('[UTIL isParityRunning] path', path);
   try {
     return axios.get('http://' + path + '/index.html')
       .then(res => true)
