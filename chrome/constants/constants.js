4136adc694dcb598424f40408ecbe10794f8cb83
goldylucks <adamgoldman7@gmail.com>
2016-07-06 16:10:09 +0200

    remove constants

--- chrome/constants/constants.js
+++
@@ -1,3 +0,0 @@
-export const SIGNER_META_TITLE = 'Parity Signer UI'; // used to focus on notificaiton click
-export const POPUP_URL = 'popup.html';
-export const WEI_TO_ETH_MULTIPLIER = 0.000000000000000001;
