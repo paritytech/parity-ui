d1f8f7785debc928d88296d820d1147119d33c88
Tomusdrw <tomusdrw@gmail.com>
2016-07-07 11:30:40 +0200

    Fixing structure of home

--- home/build_webapp.sh
+++ home/build_webapp.sh
@@ -2,18 +2,14 @@
 set -x
 set -e
 
-# This script assumes following directory structure:
-# - parity-idmanager-rs
-# - parity-dapps-home ($ git clone https://github.com/ethcore/parity-dapps-home.git)
-
-cd ../parity-dapps-home
+cd web
 rm static -R || true
 npm run build
 # Back
 cd -
 cd ./src/web
 rm -rf *
-UI=../../../parity-dapps-home/static
+UI=../../web/static
 
 cp ${UI}/inject.js .
 cp ${UI}/home.js .
