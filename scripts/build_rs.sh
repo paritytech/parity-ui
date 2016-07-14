1b8059606b38eb00c0ffbec14b772447e79a22cb
Tomusdrw <tomusdrw@gmail.com>
2016-07-14 11:57:42 +0200

    Running rust build only on master or when .rs files where modified

--- scripts/build_rs.sh
+++ scripts/build_rs.sh
@@ -5,6 +5,20 @@ set -x
 
 PROJECTS=(home signer status wallet)
 
+# For travis builds we check if there is a need to run this build
+SHOULD_RUN=true
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+  # Run build if:
+  # 1. Any .rs file was modified
+  # 2. Master branch is being built
+  RS_MODIFIED=`./scripts/modified_rs.sh`
+  SHOULD_RUN=$RS_MODIFIED || [ $TRAVIS_PULL_REQUEST = false ]
+fi
+
+if [ $SHOULD_RUN != true ]; then
+  exit 0
+fi
+
 for PROJECT in ${PROJECTS[@]}; do
   cd $PROJECT
   cargo test --no-default-features --features with-syntex
