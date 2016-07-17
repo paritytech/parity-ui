94a60d9322e61a1e7c547ef07c2f34838d0ad964
goldylucks <adamgoldman7@gmail.com>
2016-07-17 21:14:14 +0200

    (extension): extract webMessages to class, fix LS tokenChange parsing

--- extension/chrome/extension/background.js
+++ extension/chrome/extension/background.js
@@ -1,12 +1,15 @@
 import './background.html';
 import Transactions from './Transactions';
+import WebMessages from './WebMessages';
 import ProxyManager from './ProxyManager';
 import { getToken, onTokenChange } from '../utils/token';
 
 const proxyManager = new ProxyManager();
+const webMessages = new WebMessages();
 const transactions = new Transactions();
 onTokenChange(token => transactions.init(token));
 
 proxyManager.init();
+webMessages.init();
 
 getToken(token => transactions.init(token));
