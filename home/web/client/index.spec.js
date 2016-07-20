477c8de44a5eb88025f8018b2ca5cad5016cfa7b
goldylucks <adamgoldman7@gmail.com>
2016-07-20 11:40:25 +0200

    (TopBar): fix 61# failing tests

---
+++ home/web/client/index.spec.js
@@ -0,0 +1,4 @@
+// WARNING!
+// Check there is at least one more .spec.js file in client folder
+// before removing this file otherwise test would fail and build will break
+// This file currenty server as a placeholder
