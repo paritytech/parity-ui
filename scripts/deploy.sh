2b0f86f0477558d64ac2c4ab1358b08d024a7468
Tomusdrw <tomusdrw@gmail.com>
2016-07-20 12:04:28 +0200

    Fixing build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,8 @@ git clone https://${PARITY_PUSH_TOKEN}@github.com/ethcore/parity.git --depth 1
 cd parity
 cargo update -p parity-dapps
 git checkout -b auto-ui-update
+git config user.name "Travis CI"
+git config user.email "admin@ethcore.io"
 git commit -am "Bumping Parity UI"
 git push --force -u origin auto-ui-update
 
